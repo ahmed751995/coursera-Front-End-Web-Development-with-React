47e8f3ed08c1b91b6f2e68ae191b2e3dcd994f94
Ahmed Abdulrahman <ahmed751995@gmail.com>
2019-11-17 18:00:09 +0200

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent';
-import Home from './HomeComponent';
-import Contact from './ContactComponent';
-import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -39,6 +39,7 @@ class Main extends Component {
 	};
 
 	const DishWithId = ({match}) => {
+	    console.log(match);
 	    return(
 		<DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
 		  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
@@ -51,13 +52,12 @@ class Main extends Component {
               <Header />
 	      <Switch>
 		<Route path="/home" component={HomePage} />
-		<Route exact path="/menu" component={() =><Menu dishes={this.state.dishes} /> }/>
+		<Route exact path="/menu" component={() =><Menu dishes={this.state.dishes} /> } />
 		  <Route path='/menu/:dishId' component={DishWithId} />
 		  <Route exact path="/contactus" component={Contact} />
+		  <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
 		  <Redirect to="/home"/>
 	      </Switch>
-
-
 	      <Footer />
 	    </div>
 	);
